8cb0e4eae5047fd4a4fb43aed18f5e0e400ca63d
Nicolas Bernin <nicolas.bernin@reccat.io>
2023-01-11 21:07:39 +0100

    add express dotenv and morgan

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	env: {
+		development: {
+			presets: ['@babel/preset-env'],
+		},
+		production: {
+			presets: ['@babel/preset-env'],
+		},
+	},
+};
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Hello World !!!");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan(process.env.FORMAT_LOG));
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World !!!');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('404 Not Found');
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.info(`⚡ listening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode ⚡`);
+});
+
+
+
